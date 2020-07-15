f0d60a8c77f5769c4b8b5e4b2c3caa740a090cd6
Yoel Jimenez del Valle <yoel07@gmail.com>
2020-07-15 16:39:30 -0400

    Initial Commit

---
+++ Takehome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Takehome
+//
+//  Created by Yoel Jimenez del Valle on 7/15/20.
+//  Copyright © 2020 Yoel Jimenez del Valle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TakehomeTests/TakehomeTests.swift
@@ -0,0 +1,34 @@
+//
+//  TakehomeTests.swift
+//  TakehomeTests
+//
+//  Created by Yoel Jimenez del Valle on 7/15/20.
+//  Copyright © 2020 Yoel Jimenez del Valle. All rights reserved.
+//
+
+import XCTest
+@testable import Takehome
+
+class TakehomeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
